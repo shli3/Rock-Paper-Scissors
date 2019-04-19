90bf260bc070748170ee391a0451329d2a463191
shli3 <shlimoonsteven@gmail.com>
2019-04-19 21:04:56 +0100

    first commit

---
+++ Assets/Game.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+	public Text outcome;
+	public Text opponentChoice;
+	/*
+	Rock 		= 	0
+	Paper 		= 	1
+	Scissors 	= 	2
+	*/
+
+	public void Rock(){
+		//Random rng = new Random();
+		int opponent = Random.Range(0, 3);
+		System.Console.WriteLine("here");
+		textChange(opponent, "Rock");
+	}
+
+	public void Paper(){
+		//Random rng = new Random();
+		int opponent = Random.Range(0, 3);
+		textChange(opponent, "Paper");
+	}
+
+	public void Scissors(){
+		//Random rng = new Random();
+		int opponent = Random.Range(0, 3);
+		textChange(opponent, "Scissors");
+	}
+
+	public void textChange(int r, string where){
+
+		switch(where){
+			case "Rock":
+				if(r == 0){
+					outcome.text = "Tie!";
+					outcome.color = new Color(0f, 0f, 0f);
+					opponentChoice.text = "CPU: Rock";
+				}
+				else if(r == 1){
+					outcome.text = "Lose!";
+					outcome.color = new Color(255f, 0f, 0f);
+					opponentChoice.text = "CPU: Paper";
+				}
+				else{
+					outcome.text = "Win!";
+					outcome.color = new Color(0f, 255f, 0f);
+					Scoreboard.value += 1;
+					opponentChoice.text = "CPU: Scissors";
+				}
+				break;
+			case "Paper":
+				if(r == 0){
+					outcome.text = "Win!";
+					outcome.color = new Color(0f, 255f, 0f);
+					Scoreboard.value += 1;
+					opponentChoice.text = "CPU: Rock";
+				}
+				else if(r == 1){
+					outcome.text = "Tie!";
+					outcome.color = new Color(0f, 0f, 0f);
+					opponentChoice.text = "CPU: Paper";
+				}
+				else{
+					outcome.text = "Lose!";
+					outcome.color = new Color(255f, 0f, 0f);
+					opponentChoice.text = "CPU: Scissors";
+				}
+				break;
+			case "Scissors":
+				if(r == 0){
+					outcome.text = "Lose!";
+					outcome.color = new Color(255f, 0f, 0f);
+					opponentChoice.text = "CPU: Rock";
+				}
+				else if(r == 1){
+					outcome.text = "Win!";
+					outcome.color = new Color(0f, 255f, 0f);
+					Scoreboard.value += 1;
+					opponentChoice.text = "CPU: Paper";
+				}
+				else{
+					outcome.text = "Tie!";
+					outcome.color = new Color(0f, 0f, 0f);
+					opponentChoice.text = "CPU: Scissors";
+				}
+				break;
+		}
+		Scoreboard.count = Scoreboard.count + 1;
+		if(Scoreboard.count == 11){
+			Scoreboard.value = 0;
+			Scoreboard.count = 0;
+		}
+	}
+
+	
+}
---
+++ Assets/Scoreboard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scoreboard : MonoBehaviour {
+
+	public static int value = 0;
+	public static int count = 1;
+	public Text score;
+	void Start () {
+		score = GetComponent<Text>();
+	}
+	
+	void Update () {
+		score.text = "Score: " + value;
+	}
+
+	void Finale(){
+		if(count == 10){
+			if(value > 5){
+				SceneManager.LoadScene("Victory");
+			}
+			else if(value < 5){
+				SceneManager.LoadScene("Defeat");
+			}
+			else{
+				SceneManager.LoadScene("Tie");
+			}
+		}
+	}
+
+}
